6843a07cc502566fe9214cfa0b6c1b7ee55fccdc
Xsuto <michal.sutowicz@gmail.com>
2022-02-20 02:06:17 +0100

    reformatted code. Now it is easier to change criteria

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::fs;
 use std::process::exit;
 
-fn main() {
-    let file_name = "courses.txt";
-    let file = match fs::read_to_string(file_name) {
-        Ok(t) => t,
-        Err(_) => {
-            eprintln!("{file_name} does not exist");
-            exit(1);
+fn find_course(criteria: &[&str], courses: &[String]) {
+    for it in courses {
+        let is_valid_course = criteria.iter().all(|x| {
+            it.contains(x)
+        });
+        if is_valid_course {
+            println!("{it}");
         }
-    };
+    }
+}
+
+fn format_into_courses_vector(file_as_string: String) -> Vec<String> {
     let mut courses = vec![String::new()];
     let mut current_index = 0;
-    for (i, line) in file.split("\n").into_iter().enumerate() {
+    for (i, line) in file_as_string.split('\n').into_iter().enumerate() {
         if i % 6 == 0 && i >= 6 {
             courses.push(String::new());
             current_index += 1;
@@ -22,10 +25,19 @@ fn main() {
         }
         courses[current_index].push_str(line);
     }
-    println!("Kod kursu Kod grupy nazwa termin Prowadzacy Miejsca zzu stajonarne stopien");
-    for it in courses {
-        if it.contains("(pt)") && it.contains("(wt)") {
-            println!("{it}");
+    return courses;
+}
+
+fn main() {
+    let file_name = "courses.txt";
+    let file = match fs::read_to_string(file_name) {
+        Ok(t) => t,
+        Err(_) => {
+            eprintln!("{file_name} does not exist");
+            exit(1);
         }
-    }
+    };
+    let courses = format_into_courses_vector(file);
+    println!("Kod kursu Kod grupy nazwa termin Prowadzacy Miejsca zzu stajonarne stopien");
+    find_course(&["(wt)", "(pt)"], &courses);
 }
